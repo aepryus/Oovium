30e212ba45dda008853c4240d45ffa6c92893602
Joe Charlier <github@aepryus.com>
2022-03-07 23:40:06 +0900

    initial commit

---
+++ Oovium/Global/Oovium.swift
@@ -0,0 +1,14 @@
+//
+//  Oovium.swift
+//  Oovium
+//
+//  Created by Joe Charlier on 3/7/22.
+//
+
+import Foundation
+
+class Oovium {
+	func start() {
+		print("Oovium =========================================================")
+	}
+}
