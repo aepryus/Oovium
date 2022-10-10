42a9a0769a0e7a0fbc3cdd0d678d765a574fe6c2
Joe Charlier <joe@aepryus.com>
2022-10-10 16:56:20 +0900

    Explorer rotation fixed

--- Oovium/Global/OoviumController.swift
+++ Oovium/Global/OoviumController.swift
@@ -48,12 +48,16 @@ class OoviumController: UIViewController {
 	
 	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
 		super.viewWillTransition(to: size, with: coordinator)
-
-		if Screen.mac {
+        
+        if Screen.mac {
             Oovium.aetherView.frame = CGRect(x: Oovium.aetherView.left, y: Oovium.aetherView.top, width: size.width, height: size.height-Oovium.aetherView.top)
             OoviumState.behindView.frame = CGRect(origin: OoviumState.behindView.frame.origin, size: CGSize(width: OoviumState.behindView.width, height: size.height-OoviumState.behindView.top))
-		} else {
-			Oovium.aetherView.frame = CGRect(origin: .zero, size: size)
-		}
+        } else {
+            // [ Hack ] This assumes that safeTop == safeLeft in order to anticipate safeLeft before the animation occurs.  Can't use the coordinator because the Hovers get messed up. jjc 10/10/22
+            let safeLeft: CGFloat = size.width == Screen.width ? 0 : max(Screen.safeTop, Screen.safeLeft)
+            let x: CGFloat = Oovium.aetherView.left == 0 ? 0 : Screen.iPhone ? Screen.width - 20*Screen.s + safeLeft : 355*Screen.s
+            Oovium.aetherView.frame = CGRect(x: x, y: Oovium.aetherView.top, width: size.width, height: size.height-Oovium.aetherView.top)
+            OoviumState.behindView.frame = CGRect(origin: OoviumState.behindView.frame.origin, size: CGSize(width: OoviumState.behindView.width, height: size.height-OoviumState.behindView.top))
+        }
 	}
 }
