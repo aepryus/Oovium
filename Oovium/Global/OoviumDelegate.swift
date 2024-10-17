0e0c34d48e752f58016d18120f4955e0d61921eb
Joe Charlier <github@aepryus.com>
2024-10-17 16:23:58 +0900

    Cells move to Column

--- Oovium/Global/OoviumDelegate.swift
+++ Oovium/Global/OoviumDelegate.swift
@@ -48,6 +48,7 @@ class OoviumDelegate: UIResponder, UIApplicationDelegate, AetherViewDelegate {
 		let aboutAction: UIAction = UIAction(title: "About Oovium", handler: { (action: UIAction) in
 			if let backView = Oovium.aetherView.backView as? AboutView { backView.fade(aboutOn: true) }
             Oovium.aetherView.printTowers()
+            print(Oovium.aetherView.aether.unload().toJSON())
 		})
 		builder.replace(menu: .about, with: UIMenu(title: "", image: nil, identifier: .about, options: .displayInline, children: [aboutAction]))
 
