774c88704954bfecee121bae7f0d1f6ae33a205e
Joe Charlier <github@aepryus.com>
2024-11-05 22:42:03 +0900

    return and escape

--- Oovium/Global/OoviumController.swift
+++ Oovium/Global/OoviumController.swift
@@ -14,6 +14,8 @@ class OoviumController: UIViewController {
     
     init() {
         super.init(nibName: nil, bundle: nil)
+        addKeyCommand(UIKeyCommand(input: UIKeyCommand.inputEscape, modifierFlags: [], action: #selector(onEscape)))
+        addKeyCommand(UIKeyCommand(input: "\r", modifierFlags: [], action: #selector(onReturn)))
         addKeyCommand(UIKeyCommand(input: "x", modifierFlags: [.command], action: #selector(onCut)))
         addKeyCommand(UIKeyCommand(input: "c", modifierFlags: [.command], action: #selector(onCopy)))
         addKeyCommand(UIKeyCommand(input: "v", modifierFlags: [.command], action: #selector(onPaste)))
@@ -23,11 +25,11 @@ class OoviumController: UIViewController {
     required init?(coder: NSCoder) { fatalError() }
 
     func stretch() {
-		Oovium.aetherView.stretch()
-		Oovium.aetherView.needsStretch = false
-	}
+        Oovium.aetherView.stretch()
+        Oovium.aetherView.needsStretch = false
+    }
     
-// Events ==========================================================================================
+// Menus ==========================================================================================
     func onAbout() {
         if let backView = Oovium.aetherView.backView as? AboutView { backView.fade(aboutOn: true) }
         Oovium.aetherView.printTowers()
@@ -38,29 +40,23 @@ class OoviumController: UIViewController {
             Oovium.aetherView.clearAether()
         })
     }
-    @objc func onCut() {
-        Oovium.aetherView.onCut()
-    }
-    @objc func onCopy() {
-        Oovium.aetherView.onCopy()
-    }
-    @objc func onPaste() {
-        Oovium.aetherView.onPaste()
-    }
-    @objc func onUndo() {
-        Oovium.aetherView.undo()
-    }
-    @objc func onRedo() {
-        Oovium.aetherView.redo()
-    }
+
+// Keys ============================================================================================
+    @objc func onReturn() { Oovium.aetherView.onReturn() }
+    @objc func onEscape() { Oovium.aetherView.onEscape() }
+    @objc func onCut() { Oovium.aetherView.onCut() }
+    @objc func onCopy() { Oovium.aetherView.onCopy() }
+    @objc func onPaste() { Oovium.aetherView.onPaste() }
+    @objc func onUndo() { Oovium.aetherView.undo() }
+    @objc func onRedo() { Oovium.aetherView.redo() }
 
 // UIViewController ================================================================================
-	override var preferredStatusBarStyle: UIStatusBarStyle { Skin.statusBarStyle }
-	override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation { .fade }
+    override var preferredStatusBarStyle: UIStatusBarStyle { Skin.statusBarStyle }
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation { .fade }
     override var prefersHomeIndicatorAutoHidden: Bool { true }
-	
-	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-		super.viewWillTransition(to: size, with: coordinator)
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
         
         if Screen.mac {
             Oovium.aetherView.frame = CGRect(x: Oovium.aetherView.left, y: Screen.safeTop, width: size.width, height: size.height-Screen.safeTop)
@@ -75,5 +71,69 @@ class OoviumController: UIViewController {
         coordinator.animate { (context: UIViewControllerTransitionCoordinatorContext) in
             Modal.shieldView.render()
         }
-	}
+    }
 }
+
+
+
+//    class OoviumController: UIViewController {
+//        
+//        init() {
+//            super.init(nibName: nil, bundle: nil)
+//            addKeyCommand(UIKeyCommand(input: "x", modifierFlags: [.command], action: #selector(onCut)))
+//            addKeyCommand(UIKeyCommand(input: "c", modifierFlags: [.command], action: #selector(onCopy)))
+//            addKeyCommand(UIKeyCommand(input: "v", modifierFlags: [.command], action: #selector(onPaste)))
+//            addKeyCommand(UIKeyCommand(input: "z", modifierFlags: [.command], action: #selector(onUndo)))
+//            addKeyCommand(UIKeyCommand(input: "z", modifierFlags: [.command, .shift], action: #selector(onRedo)))
+//        }
+//        required init?(coder: NSCoder) { fatalError() }
+//
+//        func stretch() {
+//            Oovium.aetherView.stretch()
+//            Oovium.aetherView.needsStretch = false
+//        }
+//        
+//    // Menus ==========================================================================================
+//        func onAbout() {
+//            if let backView = Oovium.aetherView.backView as? AboutView { backView.fade(aboutOn: true) }
+//            Oovium.aetherView.printTowers()
+//            print(Oovium.aetherView.aether.unload().toJSON())
+//        }
+//        func onClear() {
+//            Oovium.aetherView.invokeConfirmModal("clearConfirm".localized, {
+//                Oovium.aetherView.clearAether()
+//            })
+//        }
+//
+//    // Keys ============================================================================================
+//        @objc func onReturn() { Oovium.aetherView.onCut() }
+//        @objc func onEscape() { Oovium.aetherView.onCut() }
+//        @objc func onCut() { Oovium.aetherView.onCut()
+//        @objc func onCopy() { Oovium.aetherView.onCopy() }
+//        @objc func onPaste() { Oovium.aetherView.onPaste() }
+//        @objc func onUndo() { Oovium.aetherView.undo() }
+//        @objc func onRedo() { Oovium.aetherView.redo() }
+//
+//    // UIViewController ================================================================================
+//        override var preferredStatusBarStyle: UIStatusBarStyle { Skin.statusBarStyle }
+//        override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation { .fade }
+//        override var prefersHomeIndicatorAutoHidden: Bool { true }
+//        
+//        override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//            super.viewWillTransition(to: size, with: coordinator)
+//            
+//            if Screen.mac {
+//                Oovium.aetherView.frame = CGRect(x: Oovium.aetherView.left, y: Screen.safeTop, width: size.width, height: size.height-Screen.safeTop)
+//                OoviumState.behindView.frame = CGRect(origin: OoviumState.behindView.frame.origin, size: CGSize(width: OoviumState.behindView.width, height: size.height-OoviumState.behindView.top))
+//            } else {
+//                // [ Hack ] This assumes that safeTop == safeLeft in order to anticipate safeLeft before the animation occurs.  Can't use the coordinator because the Hovers get messed up. jjc 10/10/22
+//                let safeLeft: CGFloat = size.width == Screen.width ? 0 : max(Screen.safeTop, Screen.safeLeft)
+//                let x: CGFloat = Oovium.aetherView.left == 0 ? 0 : Screen.iPhone ? Screen.width - 20*Screen.s + safeLeft : 355*Screen.s
+//                Oovium.aetherView.frame = CGRect(x: x, y: Oovium.aetherView.top, width: size.width, height: size.height-Oovium.aetherView.top)
+//                OoviumState.behindView.frame = CGRect(origin: OoviumState.behindView.frame.origin, size: CGSize(width: OoviumState.behindView.width, height: size.height-OoviumState.behindView.top))
+//            }
+//            coordinator.animate { (context: UIViewControllerTransitionCoordinatorContext) in
+//                Modal.shieldView.render()
+//            }
+//        }
+//    }
